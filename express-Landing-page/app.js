6d6be80ae832568a5fd4045cf097fdfc32d6b4b3
faeiz.darksiders@gmail.com <faeiz.darksiders@gmail.com>
2024-04-03 05:38:43 +0500

    .

--- express-Landing-page/app.js
+++ express-Landing-page/app.js
@@ -9,4 +9,7 @@ server.set("view engine",'ejs');
 server.get('/',function(req,res){
     res.render('index')
 })
+server.get('/Api-porsche',function(req,res){
+    res.render('crud')
+})
 server.listen(2500);
